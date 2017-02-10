6f8bd8d69bc6c5f51c21ef98f4cf648653a8259e
Elijah Fowler <j.elijah.fowler@gmail.com>
2017-02-10 11:31:29 -0600

    Added custom menu styles to work with supermenus for clean-white

--- clean-white/js/main.js
+++ clean-white/js/main.js
@@ -2,14 +2,14 @@
 // Event Calendar - Clean out styles and BR elements
 // --------------------------------------------------
 
-; (function () {
+;(function() {
 	var cal = document.querySelector('.event-cal');
 
 	if (cal) {
 		var calChild = cal.querySelectorAll('*'),
 			calBr = cal.querySelectorAll('.ec-day br');
 
-		var removeYuck = function (yuck, removeType) {
+		var removeYuck = function(yuck, removeType) {
 			if (removeType == 'attr') {
 				for (var i = 0; i < calChild.length; i++) {
 					calChild[i].removeAttribute(yuck);
@@ -31,10 +31,12 @@
 
 
 
-// 
+//
 // jQuery Scripts
 // --------------------------------------------------
 $(document).ready(function() {
+	// Add caret to menu items with drop-down
+	$('.site-nav .dropdown-toggle > a').append(' <span class="caret"></span>');
 
 	// Administration Drawer | Needs to be refoctored and moved to administration.js
 	if ($('.workflow-type select').length > 0) {
@@ -44,24 +46,24 @@ $(document).ready(function() {
 		if (startingValue == 'Live') {
 			$('.slider-switch__thumb').addClass('active refresh');
 
-			setTimeout(function () {
+			setTimeout(function() {
 				$('.slider-switch__thumb').removeClass('refresh');
 			}, 280);
 		}
 
-		$('.workflow-type > a').click(function (e) {
+		$('.workflow-type > a').click(function(e) {
 			e.preventDefault();
 
 			if (startingValue == 'Live') {
 				$('.slider-switch__thumb').removeClass('active');
 
-				setTimeout(function () {
+				setTimeout(function() {
 					$('.admin-drawer .workflow-type select').val('WorkInProgress').change();
 				}, 280);
 			} else {
 				$('.slider-switch__thumb').addClass('active');
 
-				setTimeout(function () {
+				setTimeout(function() {
 					$('.admin-drawer .workflow-type select').val('Live').change();
 				}, 280);
 			}
@@ -69,14 +71,14 @@ $(document).ready(function() {
 	}
 
 
-	// For Workflow Icons
+	// For Workflow Icons | Needs to be refactored and moved to administration.js
 	// Places better content for styling that fowards the click to original input
 	$('a.ModuleRejectContentLink').html('<i class="fa fa-ban"></i>').addClass('workflow-icon');
 
 	$('input.ModuleCancelChangesLink').each(function() {
 		$('<a class="workflow-icon" href="#"><i class="fa fa-times-circle"></i></a>').attr({
 			title: this.title
-		}).insertBefore(this).tooltip().click(function() {
+		}).insertBefore(this).uiTooltip().click(function() {
 			$(this).next().click();
 			return false;
 		});
@@ -85,7 +87,7 @@ $(document).ready(function() {
 	$('input.ModulePostDraftForApprovalLink, input.ModuleApproveContentLink').each(function() {
 		$('<a class="workflow-icon" href="#"><i class="fa fa-check-circle"></i></a>').attr({
 			title: this.title
-		}).insertBefore(this).tooltip().click(function() {
+		}).insertBefore(this).uiTooltip().click(function() {
 			$(this).next().click();
 			return false;
 		});
@@ -94,12 +96,11 @@ $(document).ready(function() {
 	$('.modulelinks img[src="/Data/SiteImages/info.gif"]').each(function() {
 		$('<a class="workflow-icon" href="#"><i class="fa fa-info-circle"></i></a>').attr({
 			title: this.title
-		}).insertBefore(this).tooltip().css('cursor', 'pointer').click(function(e) {
+		}).insertBefore(this).uiTooltip().css('cursor', 'pointer').click(function(e) {
 			e.preventDefault();
 		});;
 		return false;
 	}).remove();
-
 
 	// Remove classes from certain elements
 	$(".pollchoose .buttonlink, input[id$='_btnShowResults']").removeClass("buttonlink");
